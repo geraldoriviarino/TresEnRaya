7fd6f175a309a1ca01fe2fcb53e2dd038db0a46f
geraldoriviarino <mariogarciarb@gmail.com>
2016-12-09 16:28:36 +0100

    d

--- app/src/main/java/model/Partida.java
+++ app/src/main/java/model/Partida.java
@@ -2,13 +2,14 @@ package model;
 
 
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Copiar de la doc.
  */
 
-public class Partida {
+public class Partida implements Serializable {
 
     private final int[][] SOLUCIONES_POSIBLES = {{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9},{3,5,7}};
 
